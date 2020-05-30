0f5b4637aefe7c2534358cf4c1340d9c6dc4d97a
camilo <jcguzman8548@misena.edu.co>
2020-05-30 17:15:44 -0500

    modificacion7.1

--- JS/api.js
+++ JS/api.js
@@ -8,7 +8,7 @@ function whasta() {
    var catalogo= document.getElementById("catalogo").value;
    var observacion= document.getElementById("cuadro_obs").value;
 
-   var url= "https://api.whatsapp.com/send?phone=573057694796&text=Hola,%20este%20es%20mi%20pedido,%20gracias%20%0A"+ nombre +"%20"+ apellido +"%0A"+ direccion+"%0A*medio depago*%0A"+pago+"%0A*mi eleccion es*%0A"+catalogo+"%0A*observacion*%0A"+observacion;
+   var url= "https://api.whatsapp.com/send?phone=573057694796&text=Hola,%20este%20es%20mi%20pedido,%20gracias%20%0A"+ nombre +"%20"+ apellido +"%0A*esta es mi direccion*%0A"+ direccion+"%0A*medio depago*%0A"+pago+"%0A*mi eleccion es*%0A"+catalogo+"%0A*observacion*%0A"+observacion;
    window.open(url);
 
 
