c7b0c1d9dbf4711ce71fc98eca25433c1a19f3b4
camilo <jcguzman8548@misena.edu.co>
2020-05-31 14:41:52 -0500

    whatsapp_doñamaria

--- FrontEnd/JS/api.js
+++ FrontEnd/JS/api.js
@@ -6,7 +6,7 @@ function enviar_whatsapp() {
    var direccion= document.getElementById("cuadro_dir").value;
    var pago= document.getElementById("selec_pago").value;
    /*var catalogo= document.getElementById("catalogo").value;**/
-   var pedido="hamburguesa con doble carne, verduras, tocineta, doble queso";
+   var pedido="carne de res asada, frijoles, arroz, patacona, ensalada, huevo(opcional)";
    var observacion= document.getElementById("cuadro_obs").value;
 
    var url= "https://api.whatsapp.com/send?phone=573057694796&text=Hola,%20este%20es%20mi%20pedido,%20gracias%20%0A"+ nombre +"%20"+ apellido +"%0A*esta es mi direccion*%0A"+ direccion+"%0A*medio depago*%0A"+pago+"%0A*mi eleccion es*%0A"+pedido+"%0A*observacion*%0A"+observacion;
